13d5b7e92f28b70f94cc8d4948a0bb55f15007f5
dotomole <toydinosaurs@hotmail.com>
2019-07-27 16:03:58 +0800

    Minor layout fixes.

--- src/gui/UnitWindow.java
+++ src/gui/UnitWindow.java
@@ -8,33 +8,34 @@ import javax.swing.JScrollPane;
 
 public class UnitWindow {
 
-	private JPanel contentPane;
-	/**
-	 * Create the frame.
-	 */
-	public UnitWindow(Unit foundUnit) {
-		JFrame frame = new JFrame(foundUnit.getTitle());
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setBounds(100, 100, 639, 485);
-		
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		frame.setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(5, 7, 611, 426);
-		textPane.setBorder(UIManager.getBorder("TextArea.border"));
-		textPane.setDoubleBuffered(true);
-		textPane.setEditable(false);
-		textPane.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 13));
-		contentPane.add(textPane);
-		textPane.setText(foundUnit.toString());
-		
-		JScrollPane scrollPane = new JScrollPane(textPane);
-		scrollPane.setBounds(5, 7, 611, 426);
-		contentPane.add(scrollPane);
-		frame.setVisible(true);
-	}
-
+    private JPanel contentPane;
+    /**
+     * Create the frame.
+     */
+    public UnitWindow(Unit foundUnit) 
+    {
+        JFrame frame = new JFrame(foundUnit.getTitle());
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setBounds(100, 100, 639, 485);
+        
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        frame.setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        //Shows unit information from object's toString()
+        JTextPane textPane = new JTextPane();
+        textPane.setBounds(5, 7, 611, 426);
+        textPane.setBorder(UIManager.getBorder("TextArea.border"));
+        textPane.setDoubleBuffered(true);
+        textPane.setEditable(false);
+        textPane.setFont(new Font("MS Reference Sans Serif", Font.PLAIN, 13));
+        contentPane.add(textPane);
+        textPane.setText(foundUnit.toString());
+        
+        JScrollPane scrollPane = new JScrollPane(textPane);
+        scrollPane.setBounds(5, 7, 611, 426);
+        contentPane.add(scrollPane);
+        frame.setVisible(true);
+    }
 }
--- src/update_database/Unit.java
+++ src/update_database/Unit.java
@@ -2,75 +2,75 @@ import java.io.Serializable;
 
 public class Unit implements Serializable
 {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	//private classfields
-	private String unitCode;
-	private String title;
-	private String area;
-	private String credits;
-	private String contactHrs;
-	private String prereqs;
-	private String syllabus;
-	private String unitLeads;
+    //private classfields
+    private String unitCode;
+    private String title;
+    private String area;
+    private String credits;
+    private String contactHrs;
+    private String prereqs;
+    private String syllabus;
+    private String unitLeads;
 
-	public Unit()
-	{
+    public Unit()
+    {
 
-	}
+    }
 
-	public Unit(String unitCode, String title, String area, String credits,
-		String contactHrs, String syllabus, String prereqs, String unitLeads)
-	{
-		this.unitCode = unitCode;
-		this.title = title;
-		this.area = area;
-		this.credits = credits;
-		this.contactHrs = contactHrs;
-		this.syllabus = syllabus;
-		this.prereqs = prereqs;
-		this.unitLeads = unitLeads;
-	}
+    public Unit(String unitCode, String title, String area, String credits,
+        String contactHrs, String syllabus, String prereqs, String unitLeads)
+    {
+        this.unitCode = unitCode;
+        this.title = title;
+        this.area = area;
+        this.credits = credits;
+        this.contactHrs = contactHrs;
+        this.syllabus = syllabus;
+        this.prereqs = prereqs;
+        this.unitLeads = unitLeads;
+    }
 
-	//GETTERS
-	public String getUnitCode()
-	{
-		return unitCode;
-	}
-	public String getTitle()
-	{
-		return title;
-	}
-	public String getArea()
-	{
-		return area;
-	}
-	public String getCredits()
-	{
-		return credits;
-	}
-	public String getContactHrs()
-	{
-		return contactHrs;
-	}
-	public String getSyllabus()
-	{
-		return syllabus;
-	}
-	public String getPrereqs()
-	{
-		return prereqs;
-	}
-	public String getUnitLeads()
-	{
-		return unitLeads;
-	}
+    //GETTERS
+    public String getUnitCode()
+    {
+        return unitCode;
+    }
+    public String getTitle()
+    {
+        return title;
+    }
+    public String getArea()
+    {
+        return area;
+    }
+    public String getCredits()
+    {
+        return credits;
+    }
+    public String getContactHrs()
+    {
+        return contactHrs;
+    }
+    public String getSyllabus()
+    {
+        return syllabus;
+    }
+    public String getPrereqs()
+    {
+        return prereqs;
+    }
+    public String getUnitLeads()
+    {
+        return unitLeads;
+    }
 
-	@Override
-	public String toString()
-	{
-		return ("\nUnit Code: "+unitCode+"\nTitle: "+title+"\nArea: "+area
-			+"\nCredits: "+credits+"\nContact Hours: "+contactHrs+"\nPre-Requisites: "
-			+prereqs+"\n\nSyllabus: "+syllabus+"\n\nUnit is a pre-requisite for:\n"+unitLeads);
-	}
+    @Override
+    public String toString()
+    {
+        return ("\nUnit Code: "+unitCode+"\nTitle: "+title+"\nArea: "+area
+            +"\nCredits: "+credits+"\nContact Hours: "+contactHrs+"\nPre-Requisites: "
+            +prereqs+"\n\nSyllabus: "+syllabus+"\n\nUnit is a pre-requisite for:\n"+unitLeads);
+    }
 }
